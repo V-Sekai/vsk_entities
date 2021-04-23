7a05b12d26b9f2c828f23a5058b154d89015bb8b
Saracen <SaracenOne@gmail.com>
2021-04-23 01:52:49 +0100

    WIP hand pose support

--- extensions/player_hand_controller.gd
+++ extensions/player_hand_controller.gd
@@ -2,17 +2,8 @@ extends Node
 
 var logic_node: Node = null
 
-enum {
-	HAND_POSE_OPEN,
-	HAND_POSE_NEUTRAL,
-	HAND_POSE_POINT,
-	HAND_POSE_GUN,
-	HAND_POSE_THUMBS_UP,
-	HAND_POSE_FIST,
-	HAND_POSE_VICTORY,
-	HAND_POSE_OK_SIGN,
-	HAND_POSE_COUNT
-}
+var _avatar_display: Node = null
+export(NodePath) var avatar_display_path: NodePath = NodePath()
 
 var left_hand_gesture_id: int = 0
 var right_hand_gesture_id: int = 0
@@ -22,100 +13,114 @@ func _input(event):
 		if event.pressed:
 			match event.action:
 				"left_hand_pose_open":
-					left_hand_gesture_id = HAND_POSE_OPEN
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_OPEN
 					print("Left Hand Pose Open")
 				"left_hand_pose_neutral":
-					left_hand_gesture_id = HAND_POSE_NEUTRAL
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
 					print("Left Hand Pose Neutral")
 				"left_hand_pose_point":
-					left_hand_gesture_id = HAND_POSE_POINT
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_POINT
 					print("Left Hand Pose Point")
 				"left_hand_pose_gun":
-					left_hand_gesture_id = HAND_POSE_GUN
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_GUN
 					print("Left Hand Pose Gun")
 				"left_hand_pose_thumbs_up":
-					left_hand_gesture_id = HAND_POSE_THUMBS_UP
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_THUMBS_UP
 					print("Left Hand Pose Thumbs Up")
 				"left_hand_pose_fist":
-					left_hand_gesture_id = HAND_POSE_FIST
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_FIST
 					print("Left Hand Pose Fist")
 				"left_hand_pose_victory":
-					left_hand_gesture_id = HAND_POSE_VICTORY
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_VICTORY
 					print("Left Hand Pose Victory")
 				"left_hand_pose_ok_sign":
-					left_hand_gesture_id = HAND_POSE_OK_SIGN
+					left_hand_gesture_id = VSKAvatarManager.HAND_POSE_OK_SIGN
 					print("Left Hand Pose OK Sign")
 				"right_hand_pose_open":
-					right_hand_gesture_id = HAND_POSE_OPEN
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_OPEN
 					print("Right Hand Pose Open")
 				"right_hand_pose_neutral":
-					right_hand_gesture_id = HAND_POSE_NEUTRAL
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
 					print("Right Hand Pose Neutral")
 				"right_hand_pose_point":
-					right_hand_gesture_id = HAND_POSE_POINT
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_POINT
 					print("Right Hand Pose Point")
 				"right_hand_pose_gun":
-					right_hand_gesture_id = HAND_POSE_GUN
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_GUN
 					print("Right Hand Pose Gun")
 				"right_hand_pose_thumbs_up":
-					right_hand_gesture_id = HAND_POSE_THUMBS_UP
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_THUMBS_UP
 					print("Right Hand Pose Thumbs Up")
 				"right_hand_pose_fist":
-					right_hand_gesture_id = HAND_POSE_FIST
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_FIST
 					print("Right Hand Pose Fist")
 				"right_hand_pose_victory":
-					right_hand_gesture_id = HAND_POSE_VICTORY
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_VICTORY
 					print("Right Hand Pose Victory")
 				"right_hand_pose_ok_sign":
-					right_hand_gesture_id = HAND_POSE_OK_SIGN
+					right_hand_gesture_id = VSKAvatarManager.HAND_POSE_OK_SIGN
 					print("Right Hand Pose OK Sign")
+					
+	print(left_hand_gesture_id)
+	print(right_hand_gesture_id)
+					
+	if _avatar_display and _avatar_display.avatar_node and _avatar_display.avatar_node.driver_node:
+		_avatar_display.avatar_node.driver_node.left_hand_gesture_id = left_hand_gesture_id
+		_avatar_display.avatar_node.driver_node.right_hand_gesture_id = right_hand_gesture_id
 
 func _puppet_setup() -> void:
 	set_process_input(false)
 	
 func _master_setup() -> void:
-	left_hand_gesture_id = HAND_POSE_NEUTRAL
-	right_hand_gesture_id = HAND_POSE_NEUTRAL
+	left_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
+	right_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
 	
 	if Input.is_action_pressed("left_hand_pose_open"):
-		left_hand_gesture_id = HAND_POSE_OPEN
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_OPEN
 	elif Input.is_action_just_pressed("left_hand_pose_neutral"):
-		left_hand_gesture_id = HAND_POSE_NEUTRAL
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
 	elif Input.is_action_pressed("left_hand_pose_point"):
-		left_hand_gesture_id = HAND_POSE_POINT
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_POINT
 	elif Input.is_action_pressed("left_hand_pose_gun"):
-		left_hand_gesture_id = HAND_POSE_GUN
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_GUN
 	elif Input.is_action_pressed("left_hand_pose_thumbs_up"):
-		left_hand_gesture_id = HAND_POSE_THUMBS_UP
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_THUMBS_UP
 	elif Input.is_action_pressed("left_hand_pose_fist"):
-		left_hand_gesture_id = HAND_POSE_FIST
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_FIST
 	elif Input.is_action_pressed("left_hand_pose_victory"):
-		left_hand_gesture_id = HAND_POSE_VICTORY
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_VICTORY
 	elif Input.is_action_pressed("left_hand_pose_ok_sign"):
-		left_hand_gesture_id = HAND_POSE_OK_SIGN
+		left_hand_gesture_id = VSKAvatarManager.HAND_POSE_OK_SIGN
 
 	if Input.is_action_pressed("right_hand_pose_open"):
-		right_hand_gesture_id = HAND_POSE_OPEN
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_OPEN
 	elif Input.is_action_just_pressed("right_hand_pose_neutral"):
-		right_hand_gesture_id = HAND_POSE_NEUTRAL
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_NEUTRAL
 	elif Input.is_action_pressed("right_hand_pose_point"):
-		right_hand_gesture_id = HAND_POSE_POINT
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_POINT
 	elif Input.is_action_pressed("right_hand_pose_gun"):
-		right_hand_gesture_id = HAND_POSE_GUN
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_GUN
 	elif Input.is_action_pressed("right_hand_pose_thumbs_up"):
-		right_hand_gesture_id = HAND_POSE_THUMBS_UP
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_THUMBS_UP
 	elif Input.is_action_pressed("right_hand_pose_fist"):
-		right_hand_gesture_id = HAND_POSE_FIST
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_FIST
 	elif Input.is_action_pressed("right_hand_pose_victory"):
-		right_hand_gesture_id = HAND_POSE_VICTORY
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_VICTORY
 	elif Input.is_action_pressed("right_hand_pose_ok_sign"):
-		right_hand_gesture_id = HAND_POSE_OK_SIGN
+		right_hand_gesture_id = VSKAvatarManager.HAND_POSE_OK_SIGN
+		
+	
+	if _avatar_display and _avatar_display.avatar_node and _avatar_display.avatar_node.driver_node:
+		_avatar_display.avatar_node.driver_node.left_hand_gesture_id = left_hand_gesture_id
+		_avatar_display.avatar_node.driver_node.right_hand_gesture_id = right_hand_gesture_id
 		
 	set_process_input(true)
 
 
 func setup(p_logic_node: Node) -> void:
 	logic_node = p_logic_node
+	
+	_avatar_display = get_node_or_null(avatar_display_path)
 	
 	# State machine
 	if !logic_node.is_entity_master():
--- network/network_hands.gd
+++ network/network_hands.gd
@@ -2,8 +2,8 @@ extends NetworkLogic
 
 const math_funcs_const = preload("res://addons/math_util/math_funcs.gd")
 
-var bits: int = 0
-var transforms: Array = []
+var hand_controller: Node = null
+export (NodePath) var hand_controller_node_path = NodePath()
 
 # Degrees
 const FINGER_BASE_MIN_PITCH = -90
@@ -15,8 +15,9 @@ const DIGIT_CHAIN_MIN_PITCH = 0
 const DIGIT_CHAIN_MAX_PITCH = 90
 
 
-func on_serialize(p_writer: network_writer_const, _p_initial_state: bool) -> network_writer_const:
-	bits = 0
+func on_serialize(p_writer: network_writer_const, _p_initial_state: bool) -> network_writer_const:	
+	var left_hand_gesture_id: int = hand_controller.left_hand_gesture_id
+	var right_hand_gesture_id: int = hand_controller.right_hand_gesture_id
 
 	return p_writer
 
