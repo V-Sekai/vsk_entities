450096eb159b164e78d106fa9449059f2f7e2b2c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-01 00:56:12 -0800

    Update the infotag to use Label3D.

--- extensions/info_tag.gd
+++ extensions/info_tag.gd
@@ -2,22 +2,23 @@
 extends Node3D
 
 @export var nametag_label_nodepath: NodePath = NodePath()
+# Remains for backwards compatiblity 2022-12-01 @fire
 @export var progress_container_nodepath: NodePath = NodePath()
 @export var progress_bar_nodepath: NodePath = NodePath()
 @export var progress_label_nodepath: NodePath = NodePath()
 
-@export var nametag: String = "" :
+@export var nametag: String = "V_SEKAI_PLAYER_NAMETAG_V_SEKAI_PLAYER_NAMETAG_V_SEKAI_PLAYER_NAMETAG_V_SEKAI_PLAYER_NAMETAG_V_SEKAI_PLAYER_NAMETAG_WITH_128_CHAR" :
 	set = set_nametag
 
 
 func set_nametag(p_name: String) -> void:
 	nametag = p_name
-	var nametag_label: Label = get_node_or_null(nametag_label_nodepath)
+	var nametag_label: Label3D = get_node_or_null(nametag_label_nodepath)
 	if nametag_label:
 		nametag_label.set_text(nametag)
 		
 func show_nametag(p_show_show: bool) -> void:
-	var nametag_label: Label = get_node_or_null(nametag_label_nodepath)
+	var nametag_label: Label3D = get_node_or_null(nametag_label_nodepath)
 	if nametag_label:
 		if p_show_show:
 			nametag_label.show()
@@ -44,7 +45,7 @@ func set_background_load_stage(p_stage: int, p_stage_count: int) -> void:
 		VSKAssetManager.DOWNLOAD_PROGRESS_BAR_RATIO +
 		(VSKAssetManager.BACKGROUND_LOAD_PROGRESS_BAR_RATIO * ratio))
 	
-	var progress_label: Label = get_node_or_null(progress_label_nodepath)
+	var progress_label: Label3D = get_node_or_null(progress_label_nodepath)
 	if progress_label:
 		progress_label.set_text("%s/%s" % [str(p_stage), str(p_stage_count)])
 		
@@ -56,7 +57,7 @@ func set_download_progress(p_downloaded_bytes: int, p_body_size: int) -> void:
 	else:
 		_set_progress(0.0)
 	
-	var progress_label: Label = get_node_or_null(progress_label_nodepath)
+	var progress_label: Label3D = get_node_or_null(progress_label_nodepath)
 	if progress_label:
 		progress_label.set_text(
 			VSKAssetManager.get_download_progress_string(
